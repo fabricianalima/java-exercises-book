8b21f6ea5689207268d8f86b7afdd4f7f6ba7539
Fabriciana Lima <fabricianacorreialima@gmail.com>
2026-09-01 08:35:44 -0300

    Renderizando capitulos com thymeleaf

--- src/main/java/site/SiteController.java
+++ src/main/java/site/SiteController.java
@@ -1,5 +1,5 @@
 package site;
-
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +11,16 @@ public class SiteController {
     public String home(Model model) {
 
         model.addAttribute("nome", "Fabi");
+
+        List<String> capitulos = List.of(
+                "Introdução ao Java",
+                "Operadores",
+                "Controle de Fluxo",
+                "Collections",
+                "Exceptions"
+        );
+
+        model.addAttribute("capitulos", capitulos);
 
         return "index";
     }
